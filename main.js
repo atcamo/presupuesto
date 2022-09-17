e59a0f11f8f91f1915551ba40a182399dea7f8b3
atcamo <atiliocm@gmail.com>
2022-09-17 03:55:52 -0300

    agregue funcion

--- main.js
+++ main.js
@@ -1,36 +1,49 @@
 
 alert ('Buenos Días le ayudaremos con su presupuesto para este mes');
 
-    
-let ingresoFijo = parseInt(prompt('Cual es su ingreso fijo mensual'));
-    if (isNaN(ingresoFijo)|| ingresoFijo < 0) {
+
+/*CONDICINAL IF*/
+let ingresoFijo = parseInt(prompt('Tienes algun ingreso fijo durante el mes (ingrese un promedio)')) 
+    if (isNaN(ingresoFijo)) {
         ingresoFijo = parseInt(prompt('Cual es su ingreso fijo mensual (porfavor ingrese una cantidad en números)'));
-    }
+        }
+    else if (ingresoFijo < 0 ) {
+        ingresoFijo = parseInt(prompt('Cual es su ingreso fijo mensual (solo se puede ingresar números mayores a 0)'));
+        }
+    
+
 
 let ingresoExtra = parseInt(prompt('Tienes algun ingreso extra durante el mes (ingrese un promedio)'));
-    if (isNaN(ingresoExtra)|| ingresoExtra < 0 ) {
-        ingresoExtra = parseInt(prompt('Cual es su ingreso extra mensual(porfavor ingrese una cantidad en números)'));
+    if (isNaN(ingresoExtra)) {
+        ingresoExtra = parseInt(prompt('Cual es su ingreso extra mensual (porfavor ingrese una cantidad en números)'));
     } 
+    else if (ingresoExtra < 0 ) {
+        ingresoExtra = parseInt(prompt('Cual es su ingreso extra mensual (solo se puede ingresar números mayores a 0)'));
+        }
 
+/*FUNCION*/
+function sumaDos (primerNumero, segundoNumero) {
+    return primerNumero + segundoNumero;
+}
 
-
-
-let ingresoMes = ingresoFijo+ingresoExtra;
+let ingresoMes = sumaDos (ingresoFijo,ingresoExtra);
 
 alert ('Comencemos con los gastos (si el concepto no corresponde ponga 0)');
 
 
 let alimentacion = parseInt(prompt('Alimentación (ingrese un promedio)'));
     while (isNaN(alimentacion)|| alimentacion < 0 ) {
-        alimentacion = parseInt(prompt('Alimentación(porfavor ingrese una cantidad en números)'));
+        alimentacion = parseInt(prompt('Alimentación (porfavor ingrese una cantidad en números)'));
     }
+
 let arriendo = parseInt(prompt('Arriendo (ingrese un promedio)')); 
     while (isNaN(arriendo)|| arriendo < 0 ) {
-       arriendo = parseInt(prompt('Arriendo(porfavor ingrese una cantidad en números)'));
+       arriendo = parseInt(prompt('Arriendo (porfavor ingrese una cantidad en números)'));
     }
-let credito = parseInt(prompt('Credito(ingrese un promedio)'));
+
+let credito = parseInt(prompt('Credito (ingrese un promedio)'));
     while (isNaN(credito)|| credito < 0 ) {
-        credito = parseInt(prompt('Credito(porfavor ingrese una cantidad en números)'));
+        credito = parseInt(prompt('Credito (porfavor ingrese una cantidad en números)'));
     }
     
     
@@ -38,16 +51,14 @@ let gastosMes = alimentacion + arriendo + credito;
 
 let totalMes = ingresoMes - gastosMes;
 
-alert ('Los ingresos del mes corresponden a ' + ' ' + ingresoMes + 'Los gastos del mes corresponden a ' + ' '+ gastosMes, );
-
-alert ('el total de lo que tendra despues de pagar sus gastos sera' + ' '  + totalMes);
+alert ('Los ingresos del mes corresponden a '+ ingresoMes + '\nLos gastos del mes corresponden a '+ gastosMes + '\nEl total de lo que tendra despues de pagar sus gastos sera' + totalMes);
 
 alert ('porfavor presione el boton aceptar 3 veces');
-S
 
+/*ITERACION*/
 for (let i = 3; i > 0; i--) {
     alert (i);
 }
 
-alert ('muchas gracias');
+alert ('Muchas gracias');
 
